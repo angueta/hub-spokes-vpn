d48dc676175c5d2069a9dbfc9a59c1a1518b3bed
educho <eduardo.angueta@gmail.com>
2023-10-02 21:24:02 -0300

    edu

--- hub-spokes/main.tf
+++ hub-spokes/main.tf
@@ -738,4 +738,35 @@ resource "google_compute_instance" "vm_machine3" {
 
   }
 }
-  
+
+##########################################################
+##### Required resources for Clod NAT on PRJ-HUB
+##########################################################
+
+#### 
+resource "google_compute_router" "router-nat-hub" {
+  name    = "router-nat-hub"
+  region  = var.region
+  network = google_compute_network.hub-vpc.self_link
+  project = var.global_project_ids.hub
+}
+
+#### Creamos cloud NAT hub
+resource "google_compute_router_nat" "nat-hub" {
+  name                               = "cloud-nat-hub"
+  router = google_compute_router.router-nat-hub.name
+  region  = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  project = var.global_project_ids.hub
+  #project = google_project.prj-hub.project_id
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
+##########################################################
+##### Required resources for Clod NAT on Spokes PRJs
+##########################################################
